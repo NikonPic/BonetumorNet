96cc292ce99b77ed8dae6c9947575c27bea24c06
Nikolas Wilhelm <nikolas@wilhelm.nu>
2021-07-06 19:21:57 +0200

    extend training prep

--- src/extend_training.py
+++ src/extend_training.py
@@ -15,6 +15,8 @@ from PIL import Image, ImageDraw
 import json
 from ipywidgets import widgets
 from shapely.geometry import Polygon
+import matplotlib.pyplot as plt
+import numpy as np
 
 IMGDIR = '../PNG'
 IMGKEY = 'images'
@@ -51,17 +53,28 @@ def visualize_anno(imgdata, annodata, draw=True):
     return img
 
 
-def visualize_mask(imgdata, imgdata2, annodata, annodata2):
+def visualize_mask(imgdata, imgdata2, annodata, annodata2, highlight=False):
+    outl = 0 if highlight else None
+
+    # get both images
     original_copy = Image.open(f'{IMGDIR}/{imgdata[IMGFKEY]}')
     original_paste = Image.open(f'{IMGDIR}/{imgdata2[IMGFKEY]}')
+    # get both polygons
     poly = annodata[ANNSEGKEY][0]
     poly2 = annodata2[ANNSEGKEY][0]
+    # create new blank image
     mask = Image.new("L", original_copy.size, 0)
     draw = ImageDraw.Draw(mask)
-    draw.polygon(poly, fill=255, outline=0)
+    # draw new segmentation on blank image
+    draw.polygon(poly, fill=255, outline=outl)
+    # merge the images
     result = Image.composite(original_copy, original_paste, mask)
     draw2 = ImageDraw.Draw(result)
-    draw2.polygon(poly2, fill=None, outline=0)
+
+    print(check_poly_lim(poly, original_paste))
+
+    if highlight:
+        draw2.polygon(poly2, fill=None, outline=outl)
 
     return result
 
@@ -82,9 +95,12 @@ def calc_iou(annodata, annodata2):
     return iou
 
 
-def check_iou_lim(annodata, annodata2, lim=0.05):
+def check_iou_lim(annodata, annodata2, lim=0.01):
     """only include annotations, if th iou is lower than {lim}"""
-    iou = calc_iou(annodata, annodata2)
+    try:
+        iou = calc_iou(annodata, annodata2)
+    except:
+        return False
     if (lim >= iou):
         return True
     return False
@@ -110,27 +126,116 @@ def anno_2_poly(annodata):
     return poly
 
 
-def update(idx):
+def update(idx, highlight=False):
     imgdata = data[IMGKEY][idx]
     imgdata2 = data[IMGKEY][idx + 1]
 
     annodata = get_anno(imgdata, data)
     annodata2 = get_anno(imgdata2, data)
 
-    img = visualize_mask(imgdata, imgdata2, annodata, annodata2)
+    img = visualize_mask(imgdata, imgdata2, annodata,
+                         annodata2, highlight=highlight)
 
     inter = calc_iou(annodata, annodata2)
+    print(inter)
 
-    img.show()
+    imgarr = np.asarray(img)
+    plt.imshow(imgarr)
+
+# ok we need a logic, that imports multiple images and adds them to the paste img by randomness
 
 
-# %%
+def include_multiple_imgs(idx, maxlen=650, max_append=20, highlight=False):
+    """load img and include multiple annotations for this image randomly"""
+    # get the original img data
+    imgdata_org = data[IMGKEY][idx]
+    annodata_org = get_anno(imgdata_org, data)
+    img_paste = Image.open(f'{IMGDIR}/{imgdata_org[IMGFKEY]}')
+
+    img_arr = np.asarray(img_paste)
+    print(np.mean(img_arr))
+    mean_img = np.mean(img_arr)
+
+    # get a random number of annotations to add to the img
+    append_num = np.random.randint(1, high=max_append)
+    append_idx_list = []
+    anno_list = []
+    img_list = []
+
+    # get all idx to use and fill img and annos
+    for _ in range(append_num):
+        # take a random integer an check, that it is not already included
+        randint = np.random.randint(0, maxlen)
+
+        if randint != idx and randint not in append_idx_list:
+            append_idx_list.append(randint)
+            imgdata = data[IMGKEY][randint]
+            img_list.append(imgdata)
+            annodata = get_anno(imgdata, data)
+            anno_list.append(annodata)
+
+    # now start adding the segmnented images to the original image
+    # only if the new annotation doesnt collide with all previous annotations!
+    annos_on_img = [annodata_org]
+    for loc_imgdata, loc_annodata in zip(img_list, anno_list):
+        include = True
+        # get the local polygon
+        loc_poly = loc_annodata[ANNSEGKEY][0]
+        # only continue if the dimensions match
+        if check_poly_lim(loc_poly, img_paste):
+            # compare polygon to all previous polygons
+            for anno_on_img in annos_on_img:
+                x, y = loc_annodata['bbox'][:2]
+                print(np.mean(img_arr[x, y, :]))
+                mean_seg = np.mean(img_arr[x, y])
+
+                include = include if 2 * mean_img < mean_seg else False
+                include = include if check_iou_lim(
+                    loc_annodata, anno_on_img) else False
+        else:
+            include = False
+
+        # finally add the polygon if include remains true:
+        if include:
+            img_copy = Image.open(f'{IMGDIR}/{loc_imgdata[IMGFKEY]}')
+            mask = Image.new("L", img_copy.size, 0)
+            draw = ImageDraw.Draw(mask)
+            # draw new segmentation on blank image
+            draw.polygon(loc_poly, fill=255, outline=None)
+            img_paste = Image.composite(img_copy, img_paste, mask)
+            annos_on_img.append(loc_annodata)
+
+    if highlight:
+        draw2 = ImageDraw.Draw(img_paste)
+        for anno in annos_on_img:
+            poly_org = anno[ANNSEGKEY][0]
+            draw2.polygon(poly_org, fill=None, outline=0)
+
+        imgarr = np.asarray(img_paste)
+        plt.figure(figsize=(20, 20))
+        plt.imshow(imgarr)
+    else:
+        return img_paste, annos_on_img
+
+
+def extend_training_data(original_path='PNG', max_append=30):
+    """extend the original dataset by using copy-paste"""
+    data_extended = data.copy()
+    # update the image names:
+    for index, imgdata in enumerate(data_extended[IMGKEY]):
+        data_extended[IMGKEY][index][IMGFKEY] = f'{original_path}/{imgdata[IMGFKEY]}'
+
+    # now add new images
+    for index, _ in enumerate(data[IMGKEY]):
+        img, annos = include_multiple_imgs(
+            index, maxlen=650, max_append=max_append, highlight=False)
+
+
+        # %%
 if __name__ == '__main__':
     idx = widgets.IntSlider(0, 0, 50)
     widgets.interactive(update, idx=idx)
-# %%
-idx = widgets.IntSlider(0, 0, 50)
-widgets.interactive(update, idx=idx)
-# %%
-
-# %%
+    # %%
+    idx = widgets.IntSlider(0, 0, 50)
+    widgets.interactive(include_multiple_imgs, idx=idx)
+    # %%
